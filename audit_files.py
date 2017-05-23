33daf4e676e094d035cdd5485b1f9c8894e4cfc5
Benjamin Xiao <tiny@BenjamisMacBook.lan>
2017-05-23 00:28:25 -0700

    started writing functions, added an expected street names list

--- audit_files.py
+++ audit_files.py
@@ -16,3 +16,30 @@ and lat values are floats
 5. Correct postal code (Starts with 941 for San Francisco)
 """
 
+INPUT_FILE = "sf_sunset_district.osm"
+
+expected_street_names = ["Street", "Avenue", "Court", "Drive", "Boulevard", "Way", "Terrace", "Alley", "Place", "Lane", "Plaza", "Hill", "Circle", "Road", "Row"]
+
+def check_int(string):
+    try:
+        int(string)
+    except:
+        continue
+def check_float(string):
+    try:
+        float(string):
+    except:
+        continue
+
+def audit_street_name(elem):
+    if elem.atrrib['k'] == "addr:stret":
+        pass
+
+def audit_file(filename):
+    # first check if IDs and UIDs are integers
+    for event, elem in ET.iterparse(filename, events = ("start",)):
+        if elem.tag == "node" or elem.tag == "way":
+            print event
+            print elem
+
+audit_file(INPUT_FILE)
