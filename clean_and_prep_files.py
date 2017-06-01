8cb86afed5f49a02e8d78b5c22263ed96fc633e6
Benjamin Xiao <tiny@BenjamisMacBook.lan>
2017-05-31 23:45:35 -0700

    updated comments in cleaning file with pseudocode

--- clean_and_prep_files.py
+++ clean_and_prep_files.py
@@ -19,6 +19,8 @@ make sense
 Lastly, make a function to write your intended values to a csv file for database
 """
 
+INPUT_FILE = "san_francisco.osm"
+
 # Alameda de las Pulgas has many variations of capitalizations
 alameda_regex = re.compile(r"alameda\sde\slas\(pulgas)?", re.I)
 # if it matches this, return "Alameda de las Pulgas"
@@ -32,9 +34,6 @@ amenity, shop, or cuisine value from the auditing portion.
 
 street_corrections = {
         "Ctr": "Center",
-        "Van Ness": "Van Ness Avenue",
-        "MILLSBRAE AVE": "Millsbrae Avenue",
-        "MacDonald": "MacDonald Ave",
         "Ave": "Avenue",
         "Ave.": "Avenue",
         "Avenie": "Avenue",
@@ -51,6 +50,9 @@ street_corrections = {
          }
 
 street_replacements = {
+        "Van Ness": "Van Ness Avenue",
+        "MILLSBRAE AVE": "Millsbrae Avenue",
+        "MacDonald": "MacDonald Avenue",
         "3605 Telegraph": "3605 Telegraph Avenue",
         "Cesar Chavez St St": "Cesar Chavez Street",
         "Hyde": "Hyde Street",
@@ -62,7 +64,7 @@ amenities_corrections = {
         "car_sharing": "car_share",
         "community_centre": "community_center",
         "dancing_school": "dance_school",
-        "fountian": "fountain" # typo correction
+        "fountian": "fountain" 
         }
 
 shops_corrections = {
@@ -84,7 +86,7 @@ cuisines_corrections = {
         "coffee_shop": "coffee",
         "bubble_tea": "boba",
         "korean_food": "korean",
-        "vietnamnese": "vietnamese", # typo correction
+        "vietnamnese": "vietnamese", 
         "bbq": "barbecue",
         "doughnut": "donuts",
         "lao": "laotian"
@@ -98,6 +100,42 @@ When creating your function to clean files, make sure you write in these steps.
 4. Check to see if you need to convert to plurals
 5. Check for special cases to skip or replace
 6. Then check in dictionaries for each case
+7. Remember with subcategories, just take the first one (split on ;)
 """
 def clean_street_names(string):
+    """
+    Clean street names with the dictionaries street_corrections and street_replacments.
+    Fix capitalizations like all caps and all lower case.
+    Fix variations in capitalizations in "Alameda de las Pulgas"
+    Things in street_replacements are individually cleaned.
+    street_corrections are in the string somewhere.
+    -find its location after splitting into a list
+    -remove then replace with dictionary value
+    Remove "Wedemeyer", looks like it's a typo for Wedemeyer Bakery.
+    """
+    
+    pass
+
+def clean_value(string):
+    """
+    Takes a string as an argument
+    1. makes everything lower case
+    2. replaces empty spaces with underscores
+    3. removes underscore if it is first character
+    4. how do I check for singulars and plurals nicely?
+    5. split based on ; and take first value (but not barbecue;korean)
+    """
+    
+    pass
+
+def clean_amenities(string):
+    """
+    1. Remove addr:housenumber (the value), p, fixme, and yes
+    2. lower every string
+    3. replace spaces with "_"
+    4. split at ";" and only take first value
+    5. find singular/plurals and return plurals with plurals regex
+    6. use amenities_corretions dictionary for the typos and renaming
+    """
+
     pass
